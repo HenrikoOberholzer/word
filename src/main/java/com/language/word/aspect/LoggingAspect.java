97dc58c2c11e0dacd5d2e705bfb277a1aafd7216
Henriko Oberholzer <henriko.oberholzer@gmail.com>
2022-10-26 14:27:54 +0200

    Basic example

---
+++ src/main/java/com/language/word/aspect/LoggingAspect.java
@@ -0,0 +1,36 @@
+package com.language.word.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/*
+ Created by Henriko on 2022/10/26
+*/
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * Pointcut that matches all repositories, services and Web REST endpoints.
+     */
+    @Pointcut("within(@org.springframework.stereotype.Service *)")
+    public void springBeanPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    @Before("springBeanPointcut()")
+    public void loggingAdvice(JoinPoint joinPoint){
+        log.info("Enter: {}.{}() with argument[s] = {}", joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(), Arrays.toString(joinPoint.getArgs()));
+    }
+}
